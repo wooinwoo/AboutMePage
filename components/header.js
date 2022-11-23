8e10959b873012a9a9ca43d840ac29273be0a444
wooinwoo <wooin1001@gmail.com>
2022-11-23 18:27:40 +0900

    mod: pages-> components

--- components/header.js
+++ components/header.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import DarkModeToggleButton from "./darkModeToggleButton";
 
 export default function Header() {
   return (
@@ -9,9 +10,9 @@ export default function Header() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
             viewBox="0 0 24 24">
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
@@ -34,20 +35,8 @@ export default function Header() {
           <Link href="/project" className="mr-5 hover:text-gray-900">
             Project
           </Link>
+          <DarkModeToggleButton />
         </nav>
-        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-          Button
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            className="w-4 h-4 ml-1"
-            viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </button>
       </div>
     </header>
   );
