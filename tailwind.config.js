2f44be6db38baedcac7c1ca3cafefac4a60c1b05
wooinwoo <wooin1001@gmail.com>
2022-11-23 18:28:37 +0900

    Add: DarkModeButton 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,36 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        home: {
+          color1: "#FFF548",
+          color2: "#3C1A5B",
+        },
+        "about-me": {
+          color1: "#E2D0F9",
+          color2: "#317773",
+        },
+        timeline: {
+          color1: "#99F443",
+          color2: "#EC449B",
+        },
+        skills: {
+          color1: "green",
+          color2: "gray",
+        },
+        project: {
+          color1: "green",
+          color2: "gray",
+        },
+      },
+      secondary: {
+        bgColor: "bg-secondary-500",
+        color: "text-white",
+        outline:
+          "border-secondary-500 text-secondary-500 bg-opacity-0 hover:bg-opacity-10",
+      },
+    },
   },
   darkMode: "class",
   plugins: [],
