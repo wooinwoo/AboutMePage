eb87e487c888ea797e800fbaf0e1952bbed8024b
wooinwoo <wooin1001@gmail.com>
2022-11-23 18:28:07 +0900

    mod: timeline 블럭 추가

--- pages/timeline.js
+++ pages/timeline.js
@@ -1,53 +1,115 @@
 import Layout from "../components/layout";
 
-export default function AboutMe() {
+export default function TimeLine() {
   return (
-    <Layout>
-      <section className="text-gray-600 body-font">
-        <div className="container mx-auto flex px-5  md:flex-row flex-col">
-          <div className="lg:flex-grow lg:pr-24 md:pr-16 flex flex-col lg:flex-row md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <div>
-              <h1 className="title-font sm:text-6xl text-3xl mb-4 font-medium text-gray-900">
-                Welcome !
-              </h1>
-              <p>
-                안녕하세요! <br />
-                개발이 즐거운 프론트엔드 개발자 우인우입니다! <br />
-                <br />
-                저만의 학습 규칙을 만들어 성장하는 습관을 만드려고 노력합니다.
-                <br />
-                <br />
-                다양한 고등학교 파이썬 산학 강사 경험으로 저의 의견 잘 전달 할
-                수 있습니다.
-                <br />
-                <br />
-                개발 태도에 있어 고집이 없고 타인의 의견을 바로 수용합니다.
-                <br />
-                <br />
-                최근에는 기존 프로젝트의 성능 개선과 최신 트렌드 기술을 많이
-                접하려고 매일 학습에 매진하는 중입니다.
-                <br />
-                <br /> 앞으로 좋은 모습 보여드리겠습니다!
-              </p>
+    <Layout page={"timeline"}>
+      <section className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-5 py-10 mx-auto">
+          <div className="-my-8 divide-y-2 divide-gray-100">
+            <div className="py-8 flex flex-wrap md:flex-nowrap">
+              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span className="font-semibold title-font text-gray-700">
+                  씨엔로봇
+                </span>
+                <span className="mt-1 text-gray-500 text-sm">
+                  2021.04.19 ~ 2022.06.01
+                </span>
+              </div>
+              <div className="md:flex-grow">
+                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
+                  비터즈 해시태그 조끼 패션도끼 치아유니콘
+                </h2>
+                <p className="leading-relaxed">
+                  Glossier echo park pug, 교회의 핵심 사토리얼 바이오디젤
+                  vexillologist 팝업 스낵웨이브 램프 콘홀. Marfa 3 울프 문 파티
+                  메신저 백 셀카, 포케 베이퍼웨어 kombucha lumbersexual 삼겹살
+                  폴라로이드 후디 포틀랜드 크래프트 맥주.
+                </p>
+                <a className="text-indigo-500 inline-flex items-center mt-4">
+                  더 알아보기
+                  <svg
+                    className="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round">
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
             </div>
-            <div className="my-10">
-              <p>
-                <em>Contact.·.·</em>
-                <br /> <span className="text-rose-700">Email.</span>{" "}
-                <a href="mailto:wooin1001@gmail.com">wooin1001@gmail.com</a>{" "}
-                <br />
-                <span className="text-rose-700">Phone.</span>
-                <a href="tel:010-2324-8983">010 2324 8983</a>
-              </p>
-              <br />
-              <p>
-                <em>Channel.·.·</em>
-                <br /> <span className="text-rose-700">Blog.</span>{" "}
-                <a href="https://bhk1324.tistory.com/">bhk1324.tistory.com</a>
-                <br />
-                <span className="text-rose-700">Github.</span>{" "}
-                <a href="https://github.com/wooinwoo">github.com/wooinwoo</a>
-              </p>
+            <div className="py-8 flex flex-wrap md:flex-nowrap">
+              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span className="font-semibold title-font text-gray-700">
+                  제로베이스 프론트엔드 스쿨 4기
+                </span>
+                <span className="mt-1 text-gray-500 text-sm">
+                  2022.05.20 ~ 2022.11.20
+                </span>
+              </div>
+              <div className="md:flex-grow">
+                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
+                  명상 부시윅 직거래 박제 무당
+                </h2>
+                <p className="leading-relaxed">
+                  Glossier echo park pug, 교회의 핵심 사토리얼 바이오디젤
+                  vexillologist 팝업 스낵웨이브 램프 콘홀. Marfa 3 울프 문 파티
+                  메신저 백 셀카, 포케 베이퍼웨어 kombucha lumbersexual 삼겹살
+                  폴라로이드 후디 포틀랜드 크래프트 맥주.
+                </p>
+                <a className="text-indigo-500 inline-flex items-center mt-4">
+                  더 알아보기
+                  <svg
+                    className="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round">
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <div className="py-8 flex flex-wrap md:flex-nowrap">
+              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span className="font-semibold title-font text-gray-700">
+                  옥소폴리틱스 인턴쉽
+                </span>
+                <span className="text-sm text-gray-500">
+                  2022.09.20 ~ 2022.10.20
+                </span>
+              </div>
+              <div className="md:flex-grow">
+                <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
+                  Woke Master Cleansing 마시는 식초 샐비어
+                </h2>
+                <p className="leading-relaxed">
+                  Glossier echo park pug, 교회의 핵심 사토리얼 바이오디젤
+                  vexillologist 팝업 스낵웨이브 램프 콘홀. Marfa 3 울프 문 파티
+                  메신저 백 셀카, 포케 베이퍼웨어 kombucha lumbersexual 삼겹살
+                  폴라로이드 후디 포틀랜드 크래프트 맥주.
+                </p>
+                <a className="text-indigo-500 inline-flex items-center mt-4">
+                  더 알아보기
+                  <svg
+                    className="w-4 h-4 ml-2"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round">
+                    <path d="M5 12h14"></path>
+                    <path d="M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
+              </div>
             </div>
           </div>
         </div>
