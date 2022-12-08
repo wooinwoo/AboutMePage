779f32d49f2295be64d09da2be651dab8fe01386
wooinwoo <wooin1001@gmail.com>
2022-12-08 22:02:59 +0900

    Add: img card 배치

--- pages/skills.js
+++ pages/skills.js
@@ -3,42 +3,66 @@ const skillsValue = [
   {
     title: "Frontend",
     imgList: [
-      "https://tech.osci.kr/wp-content/uploads/2022/05/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA-2022-05-03-%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE-3.57.02.png",
+      { img: "./images/skills/html.png", width: "30%" },
+      { img: "./images/skills/CSS3.png", width: "30%" },
+      { img: "./images/skills/JavaScript.png", width: "30%" },
+      { img: "./images/skills/ts.svg", width: "40%" },
+      { img: "./images/skills/Sass.png", width: "40%" },
+      { img: "./images/skills/tailwindcss.png", width: "100%" },
+      { img: "./images/skills/React.png", width: "100%" },
+      { img: "./images/skills/Nextjs.png", width: "100%" },
+      { img: "./images/skills/jquery.png", width: "100%" },
+      { img: "./images/skills/recoil.png", width: "40%" },
+      { img: "./images/skills/Redux.png", width: "50%" },
+    ],
+  },
+  {
+    title: "Backend",
+    imgList: [{ img: "./images/skills/firebase.png", width: "100%" }],
+  },
+  {
+    title: "Deployment",
+    imgList: [
+      { img: "./images/skills/netlify.png", width: "100%" },
+      { img: "./images/skills/vercel.png", width: "100%" },
+      { img: "./images/skills/Amazon.png", width: "100%" },
+    ],
+  },
+  {
+    title: "Version Control",
+    imgList: [
+      { img: "./images/skills/git.png", width: "100%" },
+      { img: "./images/skills/github.png", width: "100%" },
     ],
   },
   {
     title: "Communication",
     imgList: [
-      "https://velog.velcdn.com/images/isitcake_yes/post/77f3d123-957e-491c-a127-01d69b93c2a8/image.png",
+      { img: "./images/skills/slack.png", width: "100%" },
+      { img: "./images/skills/figma.png", width: "100%" },
+      { img: "./images/skills/notion.png", width: "100%" },
     ],
   },
-  { title: "Deployment", imgList: [] },
-  { title: "Version Control", imgList: [] },
 ];
 
 export default function Skills() {
   return (
     <Layout page={"skills"}>
       <section class="text-gray-600 body-font overflow-hidden">
-        <div class="container px-5 pb-24 mx-auto">
-          <div class="flex flex-col text-center w-full mb-20">
-            <h1 class="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">
-              Skills
-            </h1>
-            <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-500">
-              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical.
-            </p>
-          </div>
-          <div class="flex flex-wrap -m-4">
+        <div class="container px-5 mx-auto relative">
+          <div class="flex flex-wrap flex-col content-around -m-4 h-full xl:h-[900px] lg:h-[1000px] md:h-[1500px]">
             {skillsValue.map((e) => (
-              <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
-                <div class="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
-                  <h1 class="text-3xl text-gray-600 dark:text-gray-400 pb-4 mb-4 border-b-[0.5px] border-gray-600 dark:border-gray-400 leading-none">
+              <div class="shrink-0 p-4 min-w-[300px] xl:w-1/4 lg:w-1/3 md:w-1/2 w-full">
+                <div class="h-full p-6 rounded-lg border-2 border-gray-30 dark:bg-gray-300 flex flex-col relative overflow-hidden">
+                  <h1 class="text-3xl text-gray-600 dark:text-gray-800 pb-4 mb-4 border-b-[0.5px] border-gray-600 dark:border-gray-400 leading-none">
                     {e.title}
                   </h1>
-                  <div className="flex">
+                  <div>
                     {e.imgList.map((e) => (
-                      <img src={e} />
+                      <img
+                        src={e.img}
+                        className={`inline-block w-[${e.width}] mx-[1%] hover:-translate-y-2 duration-700`}
+                      />
                     ))}
                   </div>
                 </div>
