5c402f04d5fc095f441c3b23a76a57ec3d40cd1d
wooinwoo <wooin1001@gmail.com>
2022-12-11 20:05:02 +0900

    update: height 변경

--- pages/skills.js
+++ pages/skills.js
@@ -50,7 +50,7 @@ export default function Skills() {
     <Layout page={"skills"}>
       <section class="text-gray-600 body-font overflow-hidden">
         <div class="container px-5 mx-auto relative">
-          <div class="flex flex-wrap flex-col content-around -m-4 h-full lg:h-[850px] md:h-[1500px]">
+          <div class="flex flex-wrap flex-col content-around -m-4 h-full lg:h-[850px] md:h-[1300px] mb-2 md:mb-0">
             {skillsValue.map((e) => (
               <div class="shrink-0 p-4 min-w-[300px] xl:w-1/4 lg:w-1/3 md:w-1/2 w-full">
                 <div class="h-full p-6 rounded-lg border-2 border-gray-30 dark:bg-gray-300 flex flex-col relative overflow-hidden">
