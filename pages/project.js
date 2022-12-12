e9d49e289914eac102217333af326ae2dc18e7f6
wooinwoo <wooin1001@gmail.com>
2022-12-12 23:38:23 +0900

    Simplify :  components projceList 		components projcetCard

--- pages/project.js
+++ pages/project.js
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Layout from "../components/layout";
+import ProjectCard from "../components/projectCard";
+import ProjectList from "../components/projectList";
 
 const projectValue = [
   {
@@ -103,189 +105,19 @@ const projectValue = [
 
 export default function Project() {
   const [currentImgIndex, setCurrentImgIndex] = useState(0);
-  const [style, setStyle] = useState({
-    transform: `translateX(-${currentImgIndex}00%)`,
-  });
-
-  const prevSlide = () => {
-    if (currentImgIndex !== 0) {
-      setCurrentImgIndex(currentImgIndex - 1);
-    }
-  };
-  const nextSlide = () => {
-    if (currentImgIndex + 1 !== projectValue.length) {
-      setCurrentImgIndex(currentImgIndex + 1);
-    }
-  };
-
-  useEffect(() => {
-    setStyle({
-      transform: `translateX(-${currentImgIndex}00%)`,
-    });
-  }, [currentImgIndex]);
-
   return (
     <Layout page={"project"}>
       <section className="flex flex-col lg:flex-row text-gray-600 body-font container justify-center px-5  md:px-28 mx-auto">
-        <div className=" w-[320px] md:w-[500px] relative mx-auto ">
-          <div className=" overflow-hidden">
-            <div
-              className="flex transition-all duration-700 ease-in-out"
-              style={style}>
-              {projectValue.map((e, idx) => (
-                <div key={idx}>
-                  <div className="relative card_container ">
-                    <div
-                      className="bg-gray-300 dark:bg-gray-700 p-2 rounded-lg w-[320px] md:w-[500px] h-[400px] transition-transform duration-1000 card_front"
-                      key={idx}>
-                      <img
-                        className="block rounded w-full  h-[250px] object-center mb-6"
-                        src={e.front.imgSrc}
-                        alt="content"
-                      />
-                      <h2 className="text-lg text-gray-900 font-medium title-font mb-4 dark:text-white">
-                        {e.front.title}
-                      </h2>
-                      <p className="leading-relaxed text-base text-slate-500 dark:text-slate-400">
-                        {e.front.explanation}
-                      </p>
-                    </div>
-                    <div className="flex flex-col justify-between text-black z-10 p-2 md:px-10 absolute top-0 left-0 w-full h-full bg-gray-300 dark:bg-gray-700 rounded-lg duration-1000 card_back">
-                      <div>
-                        <h2 className="flex justify-center text-xl text-gray-900 font-medium title-font mx-auto dark:text-white">
-                          {e.front.title}
-                        </h2>
-                        <p className="flex justify-center text-sm dark:text-gray-300 mb-3">
-                          {e.back.developPeriod}
-                        </p>
-                        <p className="font-bold dark:text-gray-300 mb-1">
-                          # 주요 기능 설명
-                        </p>
-                        <p className="text-sm dark:text-gray-300 mb-3">
-                          {e.back.explanation}
-                        </p>
-                        <p className="font-bold dark:text-gray-300 mb-1">
-                          # 나의 기여도
-                        </p>
-                        <nav class="flex flex-col sm:items-start sm:text-left text-center items-start -mb-1 space-y-2">
-                          {e.back.myContribution.map((e) => (
-                            <div className="flex justify-center">
-                              <span className=" bg-gray-800 text-indigo-400 w-4 h-4 mr-3 rounded-full inline-flex items-center justify-center">
-                                <svg
-                                  fill="none"
-                                  stroke="currentColor"
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  stroke-width="3"
-                                  className="w-3 h-3"
-                                  viewBox="0 0 24 24">
-                                  <path d="M20 6L9 17l-5-5"></path>
-                                </svg>
-                              </span>
-                              <span className="text-sm text-start dark:text-gray-300">
-                                {e}
-                              </span>
-                            </div>
-                          ))}
-                        </nav>
-                      </div>
-                      <div>
-                        <p className="font-bold dark:text-gray-300"># Skills</p>
-                        <div className="topBorder pb-4">
-                          {"front" in e.back.skills && (
-                            <div className="dark:text-gray-300 text-sm">
-                              <span className="font-bold mr-6">Frontend:</span>
-                              {e.back.skills.front}
-                            </div>
-                          )}
-                          {"back" in e.back.skills && (
-                            <div className="dark:text-gray-300 text-sm">
-                              <span className="font-bold mr-5">Backend:</span>
-                              {e.back.skills.back}
-                            </div>
-                          )}
-                          {"deploy" in e.back.skills && (
-                            <div className="dark:text-gray-300 text-sm">
-                              <span className="font-bold">Deployment:</span>{" "}
-                              {e.back.skills.deploy}
-                            </div>
-                          )}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="flex justify-around py-10 px-20 bg-gray-300 dark:bg-gray-700 rounded-lg mt-2 w-full h-[100px]">
-                    {e.icons.map((e, idx) => (
-                      <a href={e.href} target="_blank" key={idx}>
-                        <img className="w-[20px]" src={e.imgSrc} key={idx} />
-                      </a>
-                    ))}
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-          <button
-            className="absolute top-[45%] left-[-3rem] rounded-full text-white bg-slate-500 w-8 h-8"
-            onClick={() => prevSlide()}>
-            {"<"}
-          </button>
-          <button
-            className="absolute top-[45%] right-[-3rem] rounded-full text-white bg-slate-500 w-8 h-8"
-            onClick={() => nextSlide()}>
-            {">"}
-          </button>
-        </div>
-
-        <div className="container flex items-center justify-center mx-auto mt-24 lg:ml-24 lg:mt-10 ">
-          <div className="group text-center relative rounded-tl-lg w-[150px]">
-            <h2 className="absolute -top-20 -left-0 text-4xl text-slate-800 dark:text-slate-200">
-              Project
-            </h2>
-            <nav className="list-none bg-white dark:bg-slate-800 p-5 pr-0">
-              {projectValue.map((e, idx) => (
-                <li className="flex text-start flex-col py-2 cursor-pointer">
-                  <a
-                    className="duration-500 text-slate-500 dark:text-slate-400 hover:tracking-widest dark:hover:tracking-widest hover:text-gray-900 dark:hover:text-gray-50"
-                    onClick={() => setCurrentImgIndex(idx)}>
-                    <span className="inline-block relative">
-                      {e.front.title}
-                      <span
-                        className="inline-block absolute bottom-0 left-0 w-full h-[2px] duration-700 z-0 group-hover:z-10"
-                        style={{
-                          background:
-                            "linear-gradient(135deg,#CBCE91 0%,#76528B 50%,#CBCE91 100%)",
-                          opacity: currentImgIndex === idx ? "1" : "0",
-                        }}
-                      />
-                      <span
-                        className="inline-block absolute bottom-0 left-0 w-full h-[2px] duration-700 z-10 group-hover:z-0"
-                        style={{
-                          background:
-                            "linear-gradient(135deg,#76528B 0%,#CBCE91 50%,#76528B 100%)",
-                          opacity: currentImgIndex === idx ? "1" : "0",
-                        }}
-                      />
-                    </span>
-                  </a>
-                </li>
-              ))}
-            </nav>
-
-            <div
-              className="absolute w-[90px] h-[90px] -z-10 -top-1 -left-1 rounded-tl-lg duration-700 opacity-100 group-hover:opacity-0"
-              style={{
-                background:
-                  "linear-gradient(135deg,#CBCE91 0%,#76528B 50%,#CBCE91 100%)",
-              }}></div>
-            <div
-              className="absolute w-[90px] h-[90px] -z-10 -top-1 -left-1 rounded-tl-lg duration-700 opacity-0 group-hover:opacity-100"
-              style={{
-                background:
-                  "linear-gradient(135deg,#76528B 0%,#CBCE91 50%,#76528B 100%)",
-              }}></div>
-          </div>
-        </div>
+        <ProjectCard
+          projectValue={projectValue}
+          currentImgIndex={currentImgIndex}
+          setCurrentImgIndex={setCurrentImgIndex}
+        />
+        <ProjectList
+          projectValue={projectValue}
+          currentImgIndex={currentImgIndex}
+          setCurrentImgIndex={setCurrentImgIndex}
+        />
       </section>
     </Layout>
   );
